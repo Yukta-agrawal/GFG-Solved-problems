a2228e379caf2433e7e65344e9734e5f5651e45d
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-06-28 16:13:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
@@ -34,36 +34,75 @@ class GFG {
 class Solution {
     public int solve(int n, int m, int grid[][]) {
         // Code here
+       
         int dp[][][] = new int[n][m][m] ;
-        return helper( n , m , grid , 0 , 0 , m-1 , dp );
-    }
-    
-    public int helper(int n , int m , int grid[][] ,int r , int c1 , int c2 , int[][][] dp){
         
-        if(r == n-1){
-            if(c1==c2)
-                return grid[r][c1] ;
-            else
-                return grid[r][c1] + grid[r][c2] ;
+        for(int j1 = 0 ; j1 < m ; j1++){
+            
+            for(int j2 = 0 ; j2 < m ; j2++){
+                
+                if(j1==j2)
+                    dp[n-1][j1][j2] = grid[n-1][j1] ;
+                else
+                     dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2] ;
+                
+            }
         }
         
-        if(dp[r][c1][c2] != 0)
-            return dp[r][c1][c2] ;
-        
-        int max = 0 ;
-        for(int i = c1 -1 ; i <= c1 + 1 ; i++){
-            for(int j = c2-1 ; j<= c2 + 1 ; j++){
-                
-                if(i>=0 && j>=0 && i<m && j<m){
-                    if(c1==c2)
-                        max =  Math.max( max ,grid[r][c1] + helper( n , m , grid , r+1 ,i , j , dp));
-                    else
-                        max = Math.max( max , grid[r][c1] + grid[r][c2] +  helper( n , m , grid , r+1 ,i , j , dp));
+        for(int r = n-2 ; r>=0 ; r--){
+            for(int c1 = 0 ; c1<m ; c1++){
+                for(int c2=0 ; c2 <m ; c2++){
+                    
+                    int max = 0 ;
+                    for(int i = c1 -1 ; i <= c1 + 1 ; i++){
+                        for(int j = c2-1 ; j<= c2 + 1 ; j++){
+                            
+                            if(i>=0 && j>=0 && i<m && j<m){
+                                if(c1==c2)
+                                    max =  Math.max( max , grid[r][c1] + dp[r+1][i][j]) ;
+                                else
+                                    max = Math.max( max , grid[r][c1] + grid[r][c2] +  dp[r+1][i][j] ) ;
+                            }
+                        }
+                        
+                    }
+                    
+                    dp[r][c1][c2] = max ;
+                    
                 }
             }
-            
         }
+   
         
-        return dp[r][c1][c2] = max ;
+        return dp[0][0][m-1] ;
     }
+    
+    // public int helper(int n , int m , int grid[][] ,int r , int c1 , int c2 , int[][][] dp){
+        
+    //     if(r == n-1){
+    //         if(c1==c2)
+    //             return grid[r][c1] ;
+    //         else
+    //             return grid[r][c1] + grid[r][c2] ;
+    //     }
+        
+    //     if(dp[r][c1][c2] != 0)
+    //         return dp[r][c1][c2] ;
+        
+    //     int max = 0 ;
+    //     for(int i = c1 -1 ; i <= c1 + 1 ; i++){
+    //         for(int j = c2-1 ; j<= c2 + 1 ; j++){
+                
+    //             if(i>=0 && j>=0 && i<m && j<m){
+    //                 if(c1==c2)
+    //                     max =  Math.max( max ,grid[r][c1] + helper( n , m , grid , r+1 ,i , j , dp));
+    //                 else
+    //                     max = Math.max( max , grid[r][c1] + grid[r][c2] +  helper( n , m , grid , r+1 ,i , j , dp));
+    //             }
+    //         }
+            
+    //     }
+        
+    //     return dp[r][c1][c2] = max ;
+    // }
 }
