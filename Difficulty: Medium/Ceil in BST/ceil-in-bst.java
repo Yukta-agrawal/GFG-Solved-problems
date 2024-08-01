b750b6f77ef4f03314bc0a8fad6c7ebaaaa73da0
Yukta-agrawal <165354368+Yukta-agrawal@users.noreply.github.com>
2024-08-02 01:49:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ceil in BST/ceil-in-bst.java
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.java
@@ -109,18 +109,17 @@ class Tree {
     // Function to return the ceil of given number in BST.
     int findCeil(Node root, int key) {
         int ceil = -1;
-        
         while(root!=null){
-            if(root.data==key){
-                ceil = root.data;
-                return ceil;
+            if(root.data == key){
+                return root.data;
             }
-            if(key > root.data){
-                root = root.right;
-            }
-            else{
+            
+            if(key < root.data){
                 ceil = root.data;
                 root = root.left;
+            }
+            else{
+                root = root.right;
             }
         }
         return ceil;
